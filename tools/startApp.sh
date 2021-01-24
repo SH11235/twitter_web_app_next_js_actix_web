fe0bf64387481bc93d11b75cbac1026a5ba3379f
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2021-01-25 03:24:56 +0900

    fix ディレクトリ変更に伴うアプリ起動スクリプトの修正

--- tools/startApp.sh
+++ tools/startApp.sh
@@ -2,21 +2,21 @@
 cd `dirname $0`
 
 if [ "$1" = "start" ]; then
-	cd ../server
+	cd ../service/server/web
 	if [ -n "$(lsof -t -i:8000)" ]; then
 		kill -9 $(lsof -t -i:8000)
 	fi
-	mkdir -p log
-	nohup cargo run > log/server.log &
-	echo "server log output: server/log/server.log"
+	mkdir -p ../log
+	nohup cargo run > ../log/server.log &
+	echo "server log output: service/server/log/server.log"
 
-	cd ../client
+	cd ../../client/web
 	if [ -n "$(lsof -t -i:3000)" ]; then
 		kill -9 $(lsof -t -i:3000)
 	fi
-	mkdir -p log
-	nohup npm run start > log/client.log &
-	echo "client log output: client/log/client.log"
+	mkdir -p ../log
+	nohup npm run start > ../log/client.log &
+	echo "client log output: service/client/log/client.log"
 	echo "http://localhost:3000"
 elif [ "$1" = "stop" ]; then
 	if [ -n $(lsof -t -i:8000) ]; then
