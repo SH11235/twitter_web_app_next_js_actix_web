b539599f782cc421ee25c6767f9858fa92066cd2
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2021-01-27 00:50:21 +0900

    update アプリ起動スクリプトを修正 パスをROOT_DIRから書くようにした

--- tools/startApp.sh
+++ tools/startApp.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
-cd `dirname $0`
+#!/bin/bash
+ROOT_DIR="$(cd $(dirname $0)/..; pwd)";
 
 if [ "$1" = "start" ]; then
-	cd ../service/server/web
+	cd $ROOT_DIR/service/server/web
 	if [ -n "$(lsof -t -i:8000)" ]; then
 		kill -9 $(lsof -t -i:8000)
 	fi
-	mkdir -p ../log
-	nohup cargo run > ../log/server.log &
+	mkdir -p $ROOT_DIR/service/service/log
+	nohup cargo run > $ROOT_DIR/service/service/log/server.log &
 	echo "server log output: service/server/log/server.log"
 
-	cd ../../client/web
+	cd $ROOT_DIR/service/client/web
 	if [ -n "$(lsof -t -i:3000)" ]; then
 		kill -9 $(lsof -t -i:3000)
 	fi
-	mkdir -p ../log
-	nohup npm run start > ../log/client.log &
+	mkdir -p $ROOT_DIR/service/service/log
+	nohup npm run start > $ROOT_DIR/service/client/log/client.log &
 	echo "client log output: service/client/log/client.log"
 	echo "http://localhost:3000"
 elif [ "$1" = "stop" ]; then
