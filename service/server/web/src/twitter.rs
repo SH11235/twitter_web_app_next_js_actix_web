340915399df2e86ef3348568eaae1a5438c8ed90
highchi81 <hiraihaichiro@gmail.com>
2021-03-21 02:22:05 +0900

    add next_results

--- service/server/web/src/twitter.rs
+++ service/server/web/src/twitter.rs
@@ -12,35 +12,42 @@ struct SearchResult {
     statuses: Vec<Value>,
 }
 
-struct Twitter {}
+struct Twitter {
+    q: String,
+    count: String,
+    result_type: String,
+    origin: String,
+    bearer_token: String,
+}
 
 impl Twitter {
-    pub fn new() -> Self {
-        Twitter {}
+    pub fn new(_req: HttpRequest) -> Self {
+        // .envファイルのトークンの値を読み込む
+        dotenv().ok();
+        let query_str = _req.query_string();
+        let qs = QString::from(query_str);
+        Twitter {
+            q: qs.get("q").unwrap().to_string(),
+            count: "100".to_string(),
+            result_type: qs.get("type").unwrap().to_string(),
+            origin: match _req.headers().get("Origin") {Some(o) => o.to_str().unwrap().to_string(), None => "".to_string()},
+            bearer_token: env::var("bearer_token").expect("bearer_token is not found")
+        }
     }
 
     pub async fn search(
         &self,
-        _req: &HttpRequest,
+        next_results: &String
     ) -> Result<SearchResult, Box<dyn std::error::Error>> {
         let endpoint = "https://api.twitter.com/1.1/search/tweets.json";
         let mut headers = HeaderMap::new();
-        // .envファイルのトークンの値を読み込む
-        dotenv().ok();
-        let bearer_token = env::var("bearer_token").expect("bearer_token is not found");
         headers.insert(
             AUTHORIZATION,
-            format!("Bearer {}", bearer_token).parse().unwrap(),
+            format!("Bearer {}", &self.bearer_token).parse().unwrap(),
         );
-        let query_str = _req.query_string();
-        let qs = QString::from(query_str);
-        let q = qs.get("q").unwrap();
-        let count = "100";
-        let result_type = qs.get("type").unwrap();
-
         let client = reqwest::Client::new()
             .get(endpoint)
-            .query(&[("q", q), ("count", count), ("result_type", result_type)])
+            .query(&next_results)
             .headers(headers);
         let res: SearchResult = client.send().await?.json().await?;
         Ok(res)
@@ -48,6 +55,8 @@ impl Twitter {
 }
 
 pub async fn run_search(req: HttpRequest) -> HttpResponse {
+    let twitter = Twitter::new(req);
+
     // CORS対応
     let allowed_origin_list = [
         "http://localhost:3000",
@@ -55,30 +64,23 @@ pub async fn run_search(req: HttpRequest) -> HttpResponse {
         "http://ec2-3-135-220-104.us-east-2.compute.amazonaws.com:3000",
     ];
     let mut allow_origin = false;
-    let req_origin = match &req.headers().get("Origin") {
-        Some(o) => o.to_str().unwrap(),
-        None => ""
-    };
     for origin in allowed_origin_list.iter() {
-        if origin == &req_origin {
+        if origin == &twitter.origin {
             allow_origin = true;
             break;
         }
     }
 
     let mut values: Vec<Value> = vec![];
-    let twitter = Twitter::new();
     let mut result : Result<SearchResult, Box<dyn std::error::Error>>;
-//    let mut next_results = "";
-    let request = &req;
+    let mut next_results = format!("?q={}&count={}&result_type={}",&twitter.q,&twitter.count,&twitter.result_type);
 
-    for _ in 0..9 {
-        result = twitter.search(&request).await;
+    for _ in 0..10 {
+        result = twitter.search(&next_results).await;
         match result {
             Ok(res) => {
-//                next_results = res.search_metadata["next_results"].as_str().unwrap();
+                next_results = res.search_metadata["next_results"].as_str().unwrap().to_string();
                 values.push(Array(res.statuses));
-                // request = ???
             },
             Err(err) => println!("{:?}", err),
         }
@@ -91,6 +93,6 @@ pub async fn run_search(req: HttpRequest) -> HttpResponse {
             .header("Access-Control-Allow-Origin", "*")
             .json(values)
     } else {
-        HttpResponse::InternalServerError().body(format!("Access from origin {} has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.", req_origin))
+        HttpResponse::InternalServerError().body(format!("Access from origin {} has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.", &twitter.origin))
     }
 }
